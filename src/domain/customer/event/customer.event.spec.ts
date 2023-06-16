c623ee9dbfe9a9c9c115e35cab9acb7e760a7253
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-06-16 19:23:50 -0300

    customer created event log 2

--- src/domain/customer/event/customer.event.spec.ts
+++ src/domain/customer/event/customer.event.spec.ts
@@ -2,14 +2,18 @@ import EventDispatcher from "../../@shared/event/event-dispatcher";
 import Customer from "../entity/customer";
 import CustomerCreatedEvent from "./customer-created.event";
 import CustomerCreatedLog1Handler from "./handler/customer-created-log-1.handler";
+import CustomerCreatedLog2Handler from "./handler/customer-created-log-2.handler";
 
 describe("Customer events tests", () => {
   it("should notify when customer is created", () => {
     const eventDispatcher = new EventDispatcher();
     const eventHandler = new CustomerCreatedLog1Handler();
+    const eventHandler2 = new CustomerCreatedLog2Handler();
     const spyEventHandler = jest.spyOn(eventHandler, "handle");
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
 
     eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
 
     expect(
       eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
@@ -23,5 +27,6 @@ describe("Customer events tests", () => {
     new Customer("123", "John Doe", onCreated);
 
     expect(spyEventHandler).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
   });
 });
